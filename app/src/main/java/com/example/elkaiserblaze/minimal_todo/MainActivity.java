d47e6defdae4c21debccfcfd50b6c9f5f91bacf3
NGUYEN Phuc <phucdenlqd@gmail.com>
2018-12-24 18:15:32 +0100

    them dataPersistance

--- app/src/main/java/com/example/elkaiserblaze/minimal_todo/MainActivity.java
+++ app/src/main/java/com/example/elkaiserblaze/minimal_todo/MainActivity.java
@@ -15,12 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-        Date date1 = dateFormatter.parse("15/02/2018 18:20",new ParsePosition(0));
-        Task task1 = new Task("huyen thoai",date1);
-        persistance.addTask(task1);
-        ArrayList<Task> list_tasks = persistance.getAllTasks();
-        Log.e("Phuc",String.valueOf(list_tasks.get(list_tasks.size()-1).getTitle()));
+        
     }
 
 
