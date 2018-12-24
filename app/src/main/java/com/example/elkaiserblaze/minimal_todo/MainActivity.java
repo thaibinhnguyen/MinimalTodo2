149897c8b1a1c308b32b66fc890130029e06a560
thaibinhnguyen <thaibinhnguyen26041996@gmail.com>
2018-12-24 18:19:00 +0100

    them floating button

--- app/src/main/java/com/example/elkaiserblaze/minimal_todo/MainActivity.java
+++ app/src/main/java/com/example/elkaiserblaze/minimal_todo/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.elkaiserblaze.minimal_todo;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a =5;
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 }
