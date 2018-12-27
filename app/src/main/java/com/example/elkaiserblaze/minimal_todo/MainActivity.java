78ebbf3d9ab4e332dd1379be542263f1cabeb4ed
NGUYEN Phuc <phucdenlqd@gmail.com>
2018-12-27 18:01:42 +0100

    them chua nang xóa

--- app/src/main/java/com/example/elkaiserblaze/minimal_todo/MainActivity.java
+++ app/src/main/java/com/example/elkaiserblaze/minimal_todo/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -19,15 +23,13 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab;
     private TaskPersistance persistance= new TaskPersistance(this);
     ListView lvTask;
-    ArrayList<Task> arrayTask;
+    ArrayList<Task> arrayTask=new ArrayList<>();
     TaskAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvTask = (ListView) findViewById(R.id.listViewTask);
-        arrayTask = new ArrayList<>();
-
         //create database
         //persistance = new TaskPersistance(this,"taskNote.sqlite",null,1);
         //create table
@@ -48,12 +50,14 @@ public class MainActivity extends AppCompatActivity {
             int id = dataTask.getInt(0);
             arrayTask.add(new Task(id, title, date_task));
         }*/
-//        persistance.addTask(new Task("Lam projet", "26/12/2018 05:03"));
+        persistance.addTask(new Task("Di choi", "29/12/2018 06:03"));
         arrayTask.addAll(persistance.getAllTasks());
-        Toast.makeText(this,arrayTask.get(0).getTitle() + "",Toast.LENGTH_SHORT).show();
         adapter = new TaskAdapter(this, R.layout.layout_line, arrayTask);
         lvTask.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+
+        registerForContextMenu(lvTask);
+
         //floating button action
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -65,4 +69,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context_lv, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info= (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()){
+            case R.id.supprimer_item:
+                persistance.deleteTask(arrayTask.get(info.position).getId());
+                arrayTask.remove(info.position);
+                adapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+    }
 }
